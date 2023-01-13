1774acf2bda2cf18944c0eda4f768cdc10e39d88
Reece Elder <71394624+Reece-elder@users.noreply.github.com>
2023-01-13 10:08:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,8 @@
 favFood = [
-    "Rice Pudding",
-    "Food",
-    "Food",
-    "MoreFood",
     "yaki udon",
     "baked beans on toast",
     "cheese cake",
-    "banana and rice"
-    "Veg Puff",
-    "Ramen",
-    "pepperoni pizza",
-    "fries",
-    "pasta",
-    "margherita pizza"
 ]
-
-favDrink = [
-    "PORN STAR MARTINI",
-    "ESPRESSO MARTINI",
-    "FRENCH 75",
-    "SALTED CARAMEL OLD FASHIONED",
-    "LONG ISLAND ICED TEA",
-    "MOJITO",
-    "Bacardi Carta Blanca rum",
-    "AMARETTO SOUR",
-]
-
 
 let parent = document.querySelector("#foodList")
 
